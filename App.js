5fc9d8e7dc8c592f8d6be0b22e3bbdc434f60ade
sandeepredy6 <145992263+sandeepredy6@users.noreply.github.com>
2023-11-27 13:25:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import MyCount from './MyCount';
-  
-    function App() {
-      return (
-       < MyCount/>
-      );
-    }
+import PageComponent from './uno-components/pagecomponent';
+function App() {
+  return (
     
-    export default App;
-  
+          <div>
+              <div>
+                  <PageComponent pagevalue={100} username={ "sandeep"} > page title1</PageComponent>
+              </div>
+              <PageComponent pagevalue={101} username={  "rajesh"} > page title2</PageComponent>
+              <PageComponent pagevalue={102} username={ "harish"} > page title3</PageComponent>
+              <PageComponent pagevalue={103} username={ "diva"} > page title4</PageComponent>
+          </div>
+      );
+  }
 
+
+export default App;
